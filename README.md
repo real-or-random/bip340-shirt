6f616e8e1b7985e277261814b4ded321e5b4fc97
Tim Ruffing <me@real-or-random.org>
2024-02-29 17:42:16 +0100

    Import from gist and tidy
    History (most recent first)
      - https://gist.github.com/sipa/b50c18af3c7ee647488f322c18118ea0
      - https://gist.github.com/real-or-random/b2d457ff51be9235a1d14310bc4e7052

---
+++ README.md
@@ -0,0 +1,13 @@
+# BIP340 for Shirts
+
+An implementation of the BIP340 (Schnorr signatures) verification algorithm,
+compact enough to fit on a shirt.
+
+See [bip340_shirt.py](bip340_shirt.py) for the code.
+
+## Disclaimer
+We recommend not to use this code in production, unless your product is a shirt.
+
+## Copyright
+Except where stated otherwise, this work by Tim Ruffing and Pieter Wuille is
+made available under [CC0 1.0 Universal](COPYING.txt).
---
+++ bip340_shirt.py
@@ -0,0 +1,16 @@
+def schnorr_verify(m, p, s): # msg, pubkey, sig
+ I=int;B=lambda v:I.from_bytes(v,"big");import\
+ hashlib as h; H=lambda x: h.sha256(x).digest()
+ P=2**256-2**32-977;T=2*H(b"BIP0340/challenge")
+ L=lambda x: (c:=x**3+7,y:=pow(c,1+P//4,P),(x<P
+ and not(c-y*y)%P)and(x,-y*(-1)**y%P))[2];C=len
+ A=lambda p,q:p or q if not(p and q)else(x:=p[0
+ ],y:=p[1],f:=(q[1]-y,3*x**2,q[0]-x,2*y),l:=f[q
+ ==p]*pow(f[2+(q==p)],P-2,P),X:=(l*l-x-q[0])%P,
+ X and(X,(l*(x-X)-y)%P))[5];K=L(B(p));D=lambda\
+ x:A(x,x);M=lambda p,k:k and A(k&1 and p,D(M(p,
+ k//2)));N=P-I("j9cdktuvdh95srl1uibgen5ri",36);
+ G=D(L(I("1defdm6hgyrgjeubdk5wujq5eiukgejxf",36
+ )));S=B(s[32:]);r=B(s[:32]);t=A(M(G,S),M(K,N-B
+ (H(T+s[:32]+p+m))%N)); return K and t and(2*C(
+ p)==C(s)==64)*(r<P)*(S<N)*(t[1]&1)*(t[0]==r)>0
